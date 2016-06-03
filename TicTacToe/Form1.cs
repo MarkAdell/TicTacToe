9b37985816043c7b172d17aa52ac23bf2d78d5bd
Mark Adel <engmark_adel@hotmail.com>
2016-06-03 05:09:55 +0300

    Bug Fixes , Font editing.

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -37,7 +37,7 @@ namespace TicTacToe
 
             foreach (TextBox block in Blocks) //looping the array.
             {
-                block.Font = new Font(Font.FontFamily, 28.0F); //Setting font size.
+                block.Font = new Font("Comic Sans MS", 28.0F, FontStyle.Regular); //Editing font.
                 block.TextAlign = HorizontalAlignment.Center; //Setting the allignment.
                 block.CharacterCasing = CharacterCasing.Upper; //Setting font to UpperCase.
                 block.MaxLength = 1;
@@ -51,11 +51,11 @@ namespace TicTacToe
         int score_o = 0, score_x = 0;
 
         //Event when a TextBox text is changed.
-        private void Block_TextChanged(object sender, EventArgs e) 
+        private void Block_TextChanged(object sender, EventArgs e)
         {
             TextBox Block = (TextBox)sender; //The sender block (The event is set to all blocks).
 
-            if(Block.Text == "") return;
+            if (Block.Text == "") return;
 
             //Initializing the array.
             TextBox[,] Blocks = new TextBox[3, 3]{
@@ -64,7 +64,7 @@ namespace TicTacToe
                 {Block20 , Block21 , Block22}
             };
 
-            if(Block.Text.Length == 2)
+            if (Block.Text.Length == 2)
             {
                 char tmp = Block.Text[0];
                 Block.Text = "";
@@ -94,21 +94,19 @@ namespace TicTacToe
                 return;
             }
 
-            if(Block.Text != "") //Setting LastMove to handel wrong player turn.
+            if (Block.Text != "") //Setting LastMove to handel wrong player turn.
             {
                 LastMove = Block.Text;
             }
-            
-            for(int i = 0; i < 3; i++) //Checking vertical blocks to see if someone wins.
+
+            for (int i = 0; i < 3; i++) //Checking vertical blocks to see if someone wins.
             {
-                if(Blocks[i,0].Text != "")
+                if (Blocks[i, 0].Text != "")
                 {
-                    if (Blocks[i,0].Text.Equals(Blocks[i,1].Text) && Blocks[i,1].Text.Equals(Blocks[i,2].Text))
+                    if (Blocks[i, 0].Text.Equals(Blocks[i, 1].Text) && Blocks[i, 1].Text.Equals(Blocks[i, 2].Text))
                     {
-                        Blocks[i, 0].ForeColor = System.Drawing.Color.Red;
-                        Blocks[i, 1].ForeColor = System.Drawing.Color.Red;
-                        Blocks[i, 2].ForeColor = System.Drawing.Color.Red;
-                        Set_Score(Blocks[i,0].Text);
+                        Paint_Red(Blocks[i, 0], Blocks[i, 1], Blocks[i, 2]);
+                        Set_Score(Blocks[i, 0].Text);
                         Reset_Blocks();
                         break;
                     }
@@ -119,11 +117,9 @@ namespace TicTacToe
             {
                 if (Blocks[0, i].Text != "")
                 {
-                    if (Blocks[0,i].Text.Equals(Blocks[1,i].Text) && Blocks[1,i].Text.Equals(Blocks[2,i].Text))
+                    if (Blocks[0, i].Text.Equals(Blocks[1, i].Text) && Blocks[1, i].Text.Equals(Blocks[2, i].Text))
                     {
-                        Blocks[0, i].ForeColor = System.Drawing.Color.Red;
-                        Blocks[1, i].ForeColor = System.Drawing.Color.Red;
-                        Blocks[2, i].ForeColor = System.Drawing.Color.Red;
+                        Paint_Red(Blocks[0, i], Blocks[1, i], Blocks[2, i]);
                         Set_Score(Blocks[0, i].Text);
                         Reset_Blocks();
                         break;
@@ -131,13 +127,11 @@ namespace TicTacToe
                 }
             }
 
-            if (Blocks[1, 1].Text != "") //Checking at diagonals if someone wins.
+            if (Blocks[1, 1].Text != "") //Checking at diagonals to see if someone wins.
             {
                 if (Blocks[0, 0].Text.Equals(Blocks[1, 1].Text) && Blocks[1, 1].Text.Equals(Blocks[2, 2].Text))
                 {
-                    Blocks[0, 0].ForeColor = System.Drawing.Color.Red;
-                    Blocks[1, 1].ForeColor = System.Drawing.Color.Red;
-                    Blocks[2, 2].ForeColor = System.Drawing.Color.Red;
+                    Paint_Red(Blocks[0, 0], Blocks[1, 1], Blocks[2, 2]);
                     Set_Score(Blocks[0, 0].Text);
                     Reset_Blocks();
                     return;
@@ -145,9 +139,7 @@ namespace TicTacToe
 
                 if (Blocks[0, 2].Text.Equals(Blocks[1, 1].Text) && Blocks[1, 1].Text.Equals(Blocks[2, 0].Text))
                 {
-                    Blocks[0, 2].ForeColor = System.Drawing.Color.Red;
-                    Blocks[1, 1].ForeColor = System.Drawing.Color.Red;
-                    Blocks[2, 0].ForeColor = System.Drawing.Color.Red;
+                    Paint_Red(Blocks[0, 2], Blocks[1, 1], Blocks[2, 0]);
                     Set_Score(Blocks[0, 2].Text);
                     Reset_Blocks();
                     return;
@@ -164,21 +156,23 @@ namespace TicTacToe
             bool AllFilled = true;
             foreach (TextBox block in Blocks)
             {
-                if(block.Text == "")
+                if (block.Text == "")
                 {
                     AllFilled = false;
                     break;
                 }
             }
-            if(AllFilled == true)
+            if (AllFilled == true)
             {
                 PlaySound("blip.wav");
                 MessageBox.Show("Draw!");
                 Reset_Blocks();
             }
+
+            Block.ReadOnly = true; //Making the user unable to delete once he played.
         }
 
-        private void Set_Score(string WinningPlayer) //Method to set the scores and show winning player.
+        private void Set_Score(string WinningPlayer) //A method to set the scores and show the winning player.
         {
             PlaySound("blip.wav");
             if (WinningPlayer == "O")
@@ -197,7 +191,18 @@ namespace TicTacToe
             }
         }
 
-        private void Reset_Blocks() //Method to reset everything , called when a player wins.
+        //A method to paint the given blocks red, called when a player wins.
+        private void Paint_Red(TextBox block1, TextBox block2, TextBox block3)
+        {
+            block1.ReadOnly = false;
+            block2.ReadOnly = false;
+            block3.ReadOnly = false;
+            block1.ForeColor = System.Drawing.Color.Red;
+            block2.ForeColor = System.Drawing.Color.Red;
+            block3.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void Reset_Blocks() //A method to reset everything , called when a player wins.
         {
             LastMove = null;
 
@@ -210,13 +215,13 @@ namespace TicTacToe
             foreach (TextBox block in Blocks)
             {
                 block.ForeColor = System.Drawing.Color.Black;
+                block.ReadOnly = false;
                 block.Text = "";
             }
 
             //Unvisible label to make let it be the focused control instead of the textbox.
-            this.ActiveControl = LabelToFocusOn; 
+            this.ActiveControl = LabelToFocusOn;
         }
-
         private void PlaySound(string path) //A method to play .wav file at the sent path.
         {
             SoundPlayer simpleSound = new SoundPlayer(path);
